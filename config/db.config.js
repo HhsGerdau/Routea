7d12d28ed110b1f03542e536b2d5ad932435134e
Herlan Herbas~ <herlanherbassejas@gmail.com>
2024-11-30 00:28:58 -0400

    Primer commit

--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,7 @@ module.exports = {
     HOST: "autorack.proxy.rlwy.net",
     USER: "root",
     PASSWORD: "JTkZZOPReujSkLReRzYDRnDcrpqzfFOW",
-    DB: "apiconpaypaylpaquetes",
+    DB: "apiconpaypalpaquetes",
     dialect: "mysql",
 }
+//mysql://root:JTkZZOPReujSkLReRzYDRnDcrpqzfFOW@autorack.proxy.rlwy.net:57715/railway
