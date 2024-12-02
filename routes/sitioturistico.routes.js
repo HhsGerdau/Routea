90838e7c58fc497a33f26b6458e963921afe86e9
Herlan Herbas~ <herlanherbassejas@gmail.com>
2024-12-02 15:14:49 -0400

    Primer commit

--- routes/sitioturistico.routes.js
+++ routes/sitioturistico.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-    const controller = require("../controllers/sitiosturisticos.controller.js");
+    const controller = require("../controllers/sitioturistico.controller.js");
     let router = require("express").Router();
 
     // Listar todos los sitios turísticos
@@ -15,5 +15,5 @@ module.exports = app => {
     router.delete("/:id", controller.deleteSitioTuristico);
 
     // Registrar las rutas con el prefijo '/api/sitiosturisticos'
-    app.use('/api/sitiosturisticos', router);
+    app.use('/api/sitioturistico', router);
 };
