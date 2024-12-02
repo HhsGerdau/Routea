ee4f492cd251493ecf00dabf1b9fdae392507061
Herlan Herbas~ <herlanherbassejas@gmail.com>
2024-12-02 02:22:14 -0400

    Primer commit

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.PORT || 57715;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://routea.netlify.app'],
+    credentials:true,
 };
 app.use(cors(corsOptions));
 
